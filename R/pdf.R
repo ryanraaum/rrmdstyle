751412b0a29cb643261d55154aa3e442d913888a
Ryan Raaum <github@raaum.org>
2022-01-02 13:41:16 -0500

    implemented pdf_assignment style

--- R/pdf.R
+++ R/pdf.R
@@ -1,35 +1,73 @@
-fig_aspect = 0.618
-fig_w = 6
-fig_h = fig_w * fig_aspect
+fig_aspect <- 0.618
+fig_w <- 6
+fig_h <- fig_w * fig_aspect
 
 #' @export
-pdf_worksheet = function(
-  fig_width = fig_w, fig_height = fig_h,
-  fig_crop = TRUE, dev = "pdf", highlight = "pygments",
-  template =  template_resources("pdf", "worksheet.tex"),
-  latex_engine = "pdflatex",
+pdf_worksheet <- function(
+  fig_width = fig_w,
+  fig_height = fig_h,
+  ...
+) {
+  rr_pdf("worksheet",
+         fig_width = fig_width,
+         fig_height = fig_height,
+         ...)
+}
+
+#' @export
+pdf_assignment <- function(...) {
+  rr_pdf("assignment",...)
+}
+
+
+rr_pdf <- function(
+  doctype = c("worksheet", "assignment"),
+  fig_width = 6.5,
+  fig_height = 4.5,
+  highlight = "default",
+  template = "default",
   pandoc_args = NULL,
   ...
 ) {
+  doctype <- match.arg(doctype)
 
-  pandoc_args = c(pandoc_args,
+  if (identical(highlight, "default")) highlight <- "pygments"
+
+  if (identical(template, "default")) {
+    template <- template_resources("pdf", "worksheet.tex")
+  } else {
+    stop("User template not supported.")
+  }
+
+  pandoc_args <- c(pandoc_args,
                  paste0("--lua-filter=", template_resources("pdf", "replace_quotes.lua")),
                  paste0("--lua-filter=", template_resources("pdf", "wrap_verbatim.lua")),
                  "--variable", "fontsize=11pt",
-                 "--variable", "geometry:margin=1in",
-                 "--variable", paste0("rrlogo:", template_resources("pdf", "single-crane-logo-blackcoral.png")))
+                 "--variable", "geometry:margin=1in")
+
+  if (identical(doctype, "worksheet")) {
+    pandoc_args <- c(pandoc_args,
+                     "--variable",
+                     paste0("rrlogo:", template_resources("pdf", "single-crane-logo-blackcoral.png")),
+                     "--variable", "rrworksheet")
+  } else if (identical(doctype, "assignment")) {
+    pandoc_args <- c(pandoc_args, "--variable", "rrassignment")
+  }
 
   # use the base pdf_document format
-  format = rmarkdown::pdf_document(
-    fig_width = fig_width, fig_height = fig_height, fig_crop = fig_crop,
-    dev = dev, highlight = highlight, template = template,
-    latex_engine = latex_engine, pandoc_args = pandoc_args,...
+  format <- rmarkdown::pdf_document(
+    fig_width = fig_width,
+    fig_height = fig_height,
+    highlight = highlight,
+    template = template,
+    pandoc_args = pandoc_args,
+    ...
   )
 
-  format$knitr$opts_chunk$class.output = "bg-success"
-  format$knitr$opts_chunk$class.message = "bg-info"
-  format$knitr$opts_chunk$class.warning = "bg-warning"
-  format$knitr$opts_chunk$class.error = "bg-danger"
+  format$knitr$opts_chunk$class.output <- "bg-success"
+  format$knitr$opts_chunk$class.message <- "bg-info"
+  format$knitr$opts_chunk$class.warning <- "bg-warning"
+  format$knitr$opts_chunk$class.error <- "bg-danger"
 
   format
 }
